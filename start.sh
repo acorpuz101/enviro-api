7cc5bb07c433c005f3da543087b5acfd3bf4a1e5
acorpuz <adriancorpuz@gmail.com>
2021-02-15 06:13:08 +0000

    Added paths to start script

--- start.sh
+++ start.sh
@@ -1,2 +1,4 @@
-exec python -m flask run --host=0.0.0.0 >> logs.log &
-echo "kill $!" > shutdown.sh
+PROJECT_DIR=/home/pi/flask-app
+export FLASK_APP=$PROJECT_DIR/app.py
+exec python -m flask run --host=0.0.0.0 >> $PROJECT_DIR/logs.log &
+echo "kill $!" > $PROJECT_DIR/shutdown.sh
